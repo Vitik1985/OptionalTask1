af96fc6be82dbb72e2712515ac08cb524c72695a
Vitik1985 <viktar.vashko@gmai.com>
2021-09-29 13:47:52 +0300

    1

--- src/OptionalTask1.java
+++ src/OptionalTask1.java
@@ -1,17 +1,15 @@
-
+import static java.util.Arrays.sort;
 
 public class OptionalTask1 { //Найти самое короткое и самое длинное число. Вывести найденные числа и их длину.
     public static void main(String[] args) {
         int max;
         int min;
         int [] array_number = {22333, 1233, 12323434, 1, 21, 344, 323567};
+        sort(array_number);
 
-
-
-        if (array_1.length > array_2.length) {
-            max = array_1.length;
+        for (int i = 0; i<array_number.length; i++) {
+            System.out.print(array_number[i] + " ");
         }
-
     }
 
 }
