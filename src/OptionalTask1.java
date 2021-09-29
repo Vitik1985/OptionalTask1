3ee76e8707e8b34a02b435ed990effe6f67a9ff2
Vitik1985 <viktar.vashko@gmai.com>
2021-09-29 13:23:56 +0300

    1

--- src/OptionalTask1.java
+++ src/OptionalTask1.java
@@ -1,2 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+
 public class OptionalTask1 { //Найти самое короткое и самое длинное число. Вывести найденные числа и их длину.
+    public static void main(String[] args) throws IOException {
+        String a, b;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("enter the first number: ");
+        a = br.readLine();
+        System.out.print("enter the second number: ");
+        b = br.readLine();
+
+        char [] array_1 = a.toCharArray();
+        char [] array_2 = b.toCharArray();
+
+
+    }
+
 }
